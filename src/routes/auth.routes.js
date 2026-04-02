961b25a95321d28cd962a9204f68d4f7840394c1
Manish Kumar <whybhagat@gmail.com>
2026-04-02 11:01:48 +0530

    Delete src directory

--- src/routes/auth.routes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import { Reigster, VerfiyEmail } from '../controllers/Auth.js'
-
-const AuthRoutes=express.Router()
-
-AuthRoutes.post('/register',Reigster)
-AuthRoutes.post('/verifyEmail',VerfiyEmail)
-export default AuthRoutes
