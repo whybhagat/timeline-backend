6ff94ca5351d2540f441559ca4acef25194eb9d3
Manish Kumar <whybhagat@gmail.com>
2026-04-02 10:27:12 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
-import DbCon from "./db.js";
+import db from "../db.js";
 
 dotenv.config();
 
