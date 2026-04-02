75da31cc8894415b3109c4a724bbd5162563fbf6
Manish Kumar <whybhagat@gmail.com>
2026-04-02 10:11:25 +0530

    Add files via upload

---
+++ src/middlewares/Email.confiq.js
@@ -0,0 +1,14 @@
+import nodemailer from 'nodemailer'
+
+export const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // true for port 465, false for other ports
+  auth: {
+    user: "your Email",
+    pass: "Your Pass key",
+  },
+});
+
+
+  
