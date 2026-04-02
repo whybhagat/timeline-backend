6e0e90104a5be37b561c9205890b6b90faaa9315
Manish Kumar <whybhagat@gmail.com>
2026-04-02 11:06:32 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -17,8 +17,8 @@ DbCon();
 app.use("/auth", authRoutes);
 
 // port
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log("Server running on", PORT);
 });
