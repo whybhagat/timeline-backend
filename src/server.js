2d5f8cffca913a1d2179e8501539ceff1ac2163e
Manish Kumar <whybhagat@gmail.com>
2026-04-02 10:05:24 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,24 +1,12 @@
 import express from "express";
-import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
-import DbCon from "./db.js";
-
-dotenv.config();
 
 const app = express();
 
-// middleware
 app.use(express.json());
 
-// database connect
-DbCon();
+app.use("/api/auth", authRoutes);
 
-// routes
-app.use("/auth", authRoutes);
-
-// port
-const PORT = process.env.PORT || 4000;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(5000, () => {
+  console.log("Server running");
 });
